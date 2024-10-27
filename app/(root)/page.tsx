11bb5212b8ca9868cb60d46d7ff2b1b520ea73cb
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-27 10:52:17 +0500

    modify the folder structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
-const Home = () => {
+import SearchForm from "@/components/SearchForm";
+
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query;
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="pink_container">
+      <h1 className="heading">Welcome to the Startup Pitch App 🚀</h1>
+      <p className="sub-heading !max-w-3xl">
+        Pitch your startup idea, vote for your favorite ideas, and help them
+        grow🌱
+      </p>
+      <SearchForm query={query} />
+    </section>
   );
 };
 
