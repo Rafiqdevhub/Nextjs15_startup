add86c5f13ce5b4a1cab386a236d8241fd808449
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-10-27 14:02:34 +0500

    added the startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 const Home = async ({
   searchParams,
@@ -6,15 +7,33 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   const query = (await searchParams).query;
+
+  const posts = "useServer" as any;
   return (
-    <section className="pink_container">
-      <h1 className="heading">Welcome to the Startup Pitch App 🚀</h1>
-      <p className="sub-heading !max-w-3xl">
-        Pitch your startup idea, vote for your favorite ideas, and help them
-        grow🌱
-      </p>
-      <SearchForm query={query} />
-    </section>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Welcome to the Startup Pitch App 🚀</h1>
+        <p className="sub-heading !max-w-3xl">
+          Pitch your startup idea, vote for your favorite ideas, and help them
+          grow🌱
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 };
 
